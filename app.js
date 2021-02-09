797f1c3a1fa3b7dcaf55a97e55c711eaab5ad9d9
Ray Luna <leon.luna.ray@gmail.com>
2021-02-08 23:41:11 -0800

    starter inquirer code running

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+
+// Greet and ask user what they would like to do
+function appStart() {
+    inquirer.prompt([
+        {   type: 'list',
+            name: 'action',
+            message: 'Please select what you would like to do.',
+            choices: ['View', 'Add', 'Update']
+        }
+]).then(data => {
+    // Switch statement to run function based on the action use chose.
+    console.log(data.action)
+});
+}; // appStart
+
+appStart();
+
+// Task functions
+// Add departments, roles, employees
+// View departments, roles, employees
+// Update employee roles
